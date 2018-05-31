c5635446b463d2b1e7054f673870b76990bfe9f7
yatongzhao <yatongzhao@sohu-inc.com>
2018-05-31 20:24:57 +0800

    upload port

--- server.js
+++ server.js
@@ -116,7 +116,7 @@ app.get('*', isProd ? render : (req, res) => {
   readyPromise.then(() => render(req, res))
 })
 
-const port = 8080
+const port = isProd ? 80 : 8080
 app.listen(port, () => {
   console.log(`server started at localhost:${port}`)
 })
