e77cd9742dc212ce3e012b5eab9972f3f9653b42
yatongzhao <yatongzhao@sohu-inc.com>
2018-06-01 13:44:48 +0800

    update skill.json返回的数据中没有data字段

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -11,7 +11,7 @@ import axios from 'axios'
 
 export default {
   async asyncData () {
-    let {data} = axios({
+    let data = axios({
       method: 'get',
       url: 'http://zyt76.com/api/skill.json'
     })
