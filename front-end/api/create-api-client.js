0419a5ca410fef821186aadcc095c979e3185838
yatongzhao <yatongzhao@sohu-inc.com>
2018-06-02 19:27:01 +0800

    test cookie

--- front-end/api/create-api-client.js
+++ front-end/api/create-api-client.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
 export default axios.create({
-  xsrfCookieName: 'connect.sid',
-  xsrfHeaderName: 'X-zyt76-UID'
+  // xsrfCookieName: 'connect.sid',
+  // xsrfHeaderName: 'X-zyt76-UID'
 })
