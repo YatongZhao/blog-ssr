56c8d73a8af6166653dd3c3f444dc8f795a8f016
yatongzhao <yatongzhao@sohu-inc.com>
2018-06-05 21:55:57 +0800

    .

--- server/router/modules/API-private.js
+++ server/router/modules/API-private.js
@@ -1,4 +1,7 @@
 const express = require('express')
 const router = express.Router()
+const { API_IDENTIFY } = require('../guards/identify.js')
+
+router.use(API_IDENTIFY)
 
 module.exports = router
--- server/router/modules/API.js
+++ server/router/modules/API.js
@@ -1,8 +1,6 @@
 const express = require('express')
 const router = express.Router()
-const { API_IDENTIFY } = require('../guards/identify.js')
 
-router.all('/private', API_IDENTIFY)
 router.use('/private', require('./API-private.js'))
 router.use('/public', require('./API-public.js'))
 
