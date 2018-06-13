f9bab3f2d935983cfbddf0913ff5e3c843328ee1
yatongzhao <yatongzhao@sohu-inc.com>
2018-06-13 13:39:55 +0800

    fix fontsize

--- front-end/entry-client.js
+++ front-end/entry-client.js
@@ -2,7 +2,9 @@ import Vue from 'vue'
 import 'es6-promise/auto'
 import { createApp } from './app'
 import ProgressBar from './components/ProgressBar.vue'
-import './util/resize.js'
+import resize from './util/resize.js'
+
+resize(document, window)
 
 // global progress bar
 const bar = Vue.prototype.$bar = new Vue(ProgressBar).$mount()
